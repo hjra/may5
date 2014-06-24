32c4ef49057762dc00b4b40f7c4befa0e95f63ef
hjra <hj_r@naver.com>
2014-06-24 12:31:30 +0900

    HomeController 삭제한 내용 CustomerController에 병합

--- CShop/src/main/java/net/may5/controller/CustomerController.java
+++ CShop/src/main/java/net/may5/controller/CustomerController.java
@@ -1,5 +1,15 @@
 package net.may5.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CustomerController {
 
+	@RequestMapping(value = "cst/home/homeImage.do")
+	public String homeImage(Model model) {
+		return "cst/home/homeImage";
+
+	}
 }
