0d0e5ea06d4df30ea261af865ca4c123cc085a9d
hjra <hj_r@naver.com>
2014-06-24 12:36:09 +0900

    CustomerController 주석 추가 ManagerController 관리자페이지 링크 및 주석 추가

--- CShop/src/main/java/net/may5/controller/CustomerController.java
+++ CShop/src/main/java/net/may5/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomerController {
 
+	
+	/* 고객페이지 첫화면으로 이동 */
 	@RequestMapping(value = "cst/home/homeImage.do")
 	public String homeImage(Model model) {
 		return "cst/home/homeImage";
--- CShop/src/main/java/net/may5/controller/ManagerController.java
+++ CShop/src/main/java/net/may5/controller/ManagerController.java
@@ -1,5 +1,17 @@
 package net.may5.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ManagerController {
 
+	
+	/* 관리자페이지 첫 화면으로 이동 */
+	@RequestMapping(value ="mng/loginMng/loginForm.do")
+	public String loginForm(Model model) {
+		return "mng/loginMng/loginForm";
+
+	}
 }
