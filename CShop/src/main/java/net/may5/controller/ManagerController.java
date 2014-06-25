c59a22c3f23fd1d8e0fce4511032cb49db2a040a
hjra <hj_r@naver.com>
2014-06-25 10:25:24 +0900

    관리자페이지 - 고객정보까지 연결 완료

--- CShop/src/main/java/net/may5/controller/ManagerController.java
+++ CShop/src/main/java/net/may5/controller/ManagerController.java
@@ -3,17 +3,25 @@ package net.may5.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ManagerController {
 
-	
-	/* 관리자페이지 첫 화면으로 이동 test */
+	/** 관리자페이지 */
+	/* 관리자페이지 첫 화면으로 이동 */
 	@RequestMapping(value ="mng/loginMng/loginForm.do")
 	public String loginForm(Model model) {
 		return "mng/loginMng/loginForm";
-		
-		// test
-
 	}
+	
+	/* 로그인화면으로 이동 */
+	@RequestMapping(value="mng/loginMng/loginProcess.do"
+			, method=RequestMethod.POST)
+	public String loginProcess(Model model){
+		
+		return "mng/loginMng/loginOk"; // 실제 JSP 경로
+	}
+	
+	
 }
--- CShop/src/main/webapp/WEB-INF/views/mng/loginMng/loginForm.jsp
+++ CShop/src/main/webapp/WEB-INF/views/mng/loginMng/loginForm.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="f" uri="http://www.springframework.org/tags/form"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +8,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-관리자 페이지 첫 화면
+	관리자 페이지 첫 화면
+	<br>
+<%-- 절대경로 <f:form action="/controller/mng/loginMng/loginProcess.do" method="Post"> --%>
+	<f:form action="../loginMng/loginProcess.do" method="Post">
+		<input type="submit" value="SIGN UP">
+	</f:form>
 </body>
 </html>
