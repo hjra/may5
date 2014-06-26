a1ae4268694ba1e394ba6f2062cb2092a27a4cdd
young02020 <young02020>
2014-06-26 16:13:42 +0900

    디비접속 해서 데이터 가지고와서 보여주기

--- CShop/src/main/java/net/may5/service/ItemService.java
+++ CShop/src/main/java/net/may5/service/ItemService.java
@@ -1,4 +1,8 @@
 package net.may5.service;
 
+import net.may5.dto.Item;
+
 public interface ItemService {
+	
+	public Item getItems(String itemId);
 }
--- CShop/src/main/java/net/may5/service/ItemServiceImpl.java
+++ CShop/src/main/java/net/may5/service/ItemServiceImpl.java
@@ -1,11 +1,21 @@
 package net.may5.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import net.may5.dao.ItemMapper;
 import net.may5.dto.Item;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("itemService")
 public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private ItemMapper itemMapper;
+	
+	@Override
+	public Item getItems(String itemId) {
+		return itemMapper.getItems(itemId);
+		
+	}
 	
 }
