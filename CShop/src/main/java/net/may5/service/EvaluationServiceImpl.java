339b95427d54250c72422a03a074fb8c821e2d47
young02020 <young02020>
2014-06-30 11:12:15 +0900

    평점페이지 이동 및 홈이미지 겹치는 부분 제거

--- CShop/src/main/java/net/may5/service/EvaluationServiceImpl.java
+++ CShop/src/main/java/net/may5/service/EvaluationServiceImpl.java
@@ -1,5 +1,14 @@
 package net.may5.service;
 
+import net.may5.dao.EvaluationMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("evaluationService")
 public class EvaluationServiceImpl implements EvaluationService {
 
+	@Autowired
+	private EvaluationMapper evaluationMapper;
+	
 }
