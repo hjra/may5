b2da2e36b21e85a8bdf104410764a00b08f95bde
JoYY <JoYY@JofreeCom>
2014-07-05 15:04:25 +0900

    게시판리스트 DB에서 뽑아내기 성공

--- CShop/src/main/java/net/may5/service/QnAService.java
+++ CShop/src/main/java/net/may5/service/QnAService.java
@@ -2,9 +2,15 @@ package net.may5.service;
 
 import java.util.List;
 
+
 import net.may5.dto.QnA;
 
 public interface QnAService {
 
-	public List<QnA> getQnaList();
+	public List<QnA> qnaGetList();
+	public List<QnA> qnaGetList2();
+	void insertQnA(QnA qna);
+	QnA getQnA(int boardCode);
+	void updateQnA(QnA qna);
+	void deleteQnA(int boardCode);
 }
